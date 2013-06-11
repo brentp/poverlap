56763b2bab0c40997cc6f5be280ce623ed306ac0
Brent Pedersen <bpederse@gmail.com>
2013-06-11 14:52:01 -0600

    scripts to get segmentations

---
+++ data/get-chrom-hmm.sh
@@ -0,0 +1,17 @@
+# taken from @arq5x
+set -ex
+cell_types=(Gm12878 H1hesc Hepg2 Hmec Hsmm Huvec K562 Nhek Nhlf)
+dir=data/chromHMM
+
+mkdir -p $dir; cd $dir;
+
+files=""
+for ct in "${cell_types[@]}"; do
+    echo $ct;
+    remote=http://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeBroadHmm/wgEncodeBroadHmm${ct}HMM.bed.gz
+    F=$(basename $remote .gz)
+    #wget --quiet -O - $remote | zcat - | cut -f 1-4 | perl -pe 's/\d+_(.+)/$1/' > $F
+    files="$files $F"
+done
+ 
+bedtools unionbedg -header -names "${cell_types[@]}" -i $files > master.chromhmm.bedg
---
+++ data/get-combined.sh
@@ -0,0 +1,21 @@
+# from: https://gist.github.com/arq5x/3719100
+set -ex
+echo "bigBedToBed must be on \$PATH"
+echo "http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/bigBedToBed"
+
+cell_types=(gm12878 h1hesc helas3 hepg2 huvec k562) 
+dir=data/combined
+mkdir -p $dir; cd $dir;
+
+
+files=""
+for ct in "${cell_types[@]}"; do
+    echo $ct;
+    bb="http://ftp.ebi.ac.uk/pub/databases/ensembl/encode/awgHub/byDataType/segmentations/jan2011/${ct}.combined.bb"
+    #wget --quiet $bb;
+    F=$(basename $bb .bb)
+    #bigBedToBed $(basename $bb) stdout | cut -f 1-4 | gzip -c > ${F}.bedg.gz
+    files="$files ${F}.bedg.gz"
+done
+ 
+bedtools unionbedg -header -names "${cell_types[@]}" -i $files > master.chromhmm.bedg
