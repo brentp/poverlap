50222b4875cd3bd7f13e10b83157c7e2989ee0fa
Brent Pedersen <bpederse@gmail.com>
2013-06-23 15:13:51 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 
-
 version = '0.1'
 
 install_requires = [
     'nose',
 ]
-
 
 setup(name='poverlap',
     version=version,
@@ -27,14 +25,14 @@ setup(name='poverlap',
     author_email='bpederse@gmail.com',
     url='https://github.com/brentp/poverlap/',
     license='BSD (2-clause)',
-    packages=['.'],
+    #packages=['.'],
     include_package_data=True,
     test_suite='nose.collector',
     zip_safe=False,
     install_requires=install_requires,
     requires=['commandr'],
-    entry_points={
-        'console_scripts':
-            ['poverlap=poverlap:main']
-    }
+    #entry_points={
+    #    'console_scripts':
+    #        ['poverlap=poverlap:main']
+    #}
 )
