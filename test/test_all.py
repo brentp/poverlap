86c94d3ed910069f60a56d16e107534f5fd69901
Brent Pedersen <bpederse@gmail.com>
2013-06-27 11:02:55 -0600

    test for local shuffle

--- test/test_all.py
+++ test/test_all.py
@@ -28,6 +28,12 @@ def test_string_metric():
     assert isinstance(res, basestring)
     yield check_attributes, res
 
+def test_local():
+    res = poverlap('test/data/a.bed', 'test/data/b.bed', 'data/hg19.genome',
+            shuffle_loc=100, n=20)
+    assert isinstance(res, basestring)
+    yield check_attributes, res
+
 def test_fixle():
     res = fixle('test/data/haim.test.bed', 'CTCF', 'Pol2', n=20)
     d = json.loads(res)
